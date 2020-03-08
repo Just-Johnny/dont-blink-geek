08f38187637e18d1dd6d175a51ab84f5ba375bce
John Burks <jmburks@gmail.com>
2020-03-07 22:02:40 -0600

    Update year.

--- dist/js/detects.js
+++ dist/js/detects.js
@@ -1,7 +1,7 @@
 /*!
  * dont-blink-geek v1.0.0
  * Website for Don't Blink Geek.
- * (c) 2019 John Burks
+ * (c) 2020 John Burks
  * MIT License
  * https://github.com/Just-Johnny/dont-blink-geek
  */
